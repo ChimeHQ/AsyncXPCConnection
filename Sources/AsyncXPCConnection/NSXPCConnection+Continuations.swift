d7f6ec40d9f050fef87c25ab67ba5f626e4d4dee
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-01-20 07:02:26 -0500

    isolated param versions, fix a few missing function params

--- Sources/AsyncXPCConnection/NSXPCConnection+Continuations.swift
+++ Sources/AsyncXPCConnection/NSXPCConnection+Continuations.swift
@@ -7,6 +7,26 @@ enum ConnectionContinuationError: Error {
 
 extension NSXPCConnection {
 	/// Begins remote method invocation that returns a value.
+	public func withContinuation<Service, Value>(
+		function: String = #function,
+		on actor: isolated some Actor,
+		_ body: (Service, CheckedContinuation<Value, Error>) -> Void
+	) async throws -> Value {
+		return try await withCheckedThrowingContinuation(function: function) { continuation in
+			let proxy = self.remoteObjectProxyWithErrorHandler { error in
+				continuation.resume(throwing: error)
+			}
+
+			guard let service = proxy as? Service else {
+				continuation.resume(throwing: ConnectionContinuationError.serviceTypeMismatch)
+				return
+			}
+
+			body(service, continuation)
+		}
+	}
+
+	/// Begins remote method invocation that returns a value.
 	@_unsafeInheritExecutor
 	public func withContinuation<Service, Value>(
 		function: String = #function,
@@ -44,6 +64,25 @@ extension NSXPCConnection {
 			}
 		})
 	}
+
+	/// Begins remote method invocation.
+	///
+	/// Even though the remote call does not return errors, this function still throws because communication can always fail.
+	public func withService<Service>(
+		function: String = #function,
+		on actor: isolated some Actor,
+		_ body: (Service) throws -> Void
+	) async throws {
+		try await withContinuation(function: function, on: actor, { (service: Service, continuation: CheckedContinuation<Void, Error>) in
+			do {
+				try body(service)
+
+				continuation.resume()
+			} catch {
+				continuation.resume(throwing: error)
+			}
+		})
+	}
 }
 
 extension NSXPCConnection {
@@ -55,7 +94,31 @@ extension NSXPCConnection {
 		function: String = #function,
 		_ body: (Service, @escaping @Sendable (Value?, Error?) -> Void) -> Void
 	) async throws -> Value {
-		try await withContinuation { service, continuation in
+		try await withContinuation(function: function) { service, continuation in
+			body(service) { value, error in
+				switch (value, error) {
+				case let (value?, nil):
+					continuation.resume(returning: value)
+				case let (nil, error?):
+					continuation.resume(throwing: error)
+				case let (_, error?):
+					continuation.resume(throwing: error)
+				case (nil, nil):
+					continuation.resume(throwing: ConnectionContinuationError.missingBothValueAndError)
+				}
+			}
+		}
+	}
+
+	/// Begins remote method invocation that calls out to a value-error pair completion handler.
+	///
+	/// This function always throws if an error is returned from the completion handler.
+	public func withValueErrorCompletion<Service, Value: Sendable>(
+		function: String = #function,
+		on actor: isolated some Actor,
+		_ body: (Service, @escaping @Sendable (Value?, Error?) -> Void) -> Void
+	) async throws -> Value {
+		try await withContinuation(function: function, on: actor) { service, continuation in
 			body(service) { value, error in
 				switch (value, error) {
 				case let (value?, nil):
@@ -77,7 +140,20 @@ extension NSXPCConnection {
 		function: String = #function,
 		_ body: (Service, @escaping @Sendable (Result<Value, Error>) -> Void) -> Void
 	) async throws -> Value {
-		try await withContinuation { service, continuation in
+		try await withContinuation(function: function) { service, continuation in
+			body(service) { result in
+				continuation.resume(with: result)
+			}
+		}
+	}
+
+	/// Begins remote method invocation that calls out to a Result-based completion handler.
+	public func withResultCompletion<Service, Value: Sendable>(
+		function: String = #function,
+		on actor: isolated some Actor,
+		_ body: (Service, @escaping @Sendable (Result<Value, Error>) -> Void) -> Void
+	) async throws -> Value {
+		try await withContinuation(function: function, on: actor) { service, continuation in
 			body(service) { result in
 				continuation.resume(with: result)
 			}
@@ -90,7 +166,24 @@ extension NSXPCConnection {
 		function: String = #function,
 		_ body: (Service, @escaping @Sendable (Error?) -> Void) -> Void
 	) async throws {
-		try await withContinuation { (service, continuation: CheckedContinuation<Void, Error>) in
+		try await withContinuation(function: function) { (service, continuation: CheckedContinuation<Void, Error>) in
+			body(service) { error in
+				if let error = error {
+					continuation.resume(throwing: error)
+				} else {
+					continuation.resume()
+				}
+			}
+		}
+	}
+
+	/// Begins remote method invocation that calls out to a failable completion handler.
+	public func withErrorCompletion<Service>(
+		function: String = #function,
+		on actor: isolated some Actor,
+		_ body: (Service, @escaping @Sendable (Error?) -> Void) -> Void
+	) async throws {
+		try await withContinuation(function: function, on: actor) { (service, continuation: CheckedContinuation<Void, Error>) in
 			body(service) { error in
 				if let error = error {
 					continuation.resume(throwing: error)
@@ -106,7 +199,19 @@ extension NSXPCConnection {
 		function: String = #function,
 		_ body: (Service, @escaping @Sendable (Data?, Error?) -> Void) -> Void
 	) async throws -> Value {
-		let data: Data = try await withValueErrorCompletion { service, handler in
+		let data: Data = try await withValueErrorCompletion(function: function) { service, handler in
+			body(service, handler)
+		}
+
+		return try JSONDecoder().decode(Value.self, from: data)
+	}
+
+	public func withDecodingCompletion<Service, Value: Decodable>(
+		function: String = #function,
+		on actor: isolated some Actor,
+		_ body: (Service, @escaping @Sendable (Data?, Error?) -> Void) -> Void
+	) async throws -> Value {
+		let data: Data = try await withValueErrorCompletion(function: function, on: actor) { service, handler in
 			body(service, handler)
 		}
 
